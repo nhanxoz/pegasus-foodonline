893d63f2c190efd7c1b55ddc673ac37fe8c6fa9d
nhanxoz <nhan9ckl@gmail.com>
2022-04-30 07:09:10 +0700

    first commit

---
+++ src/main/java/com/pegasus/foodonline/sqlserver/SpringBootJdbctemplateSqlServerApplication.java
@@ -0,0 +1,13 @@
+package com.pegasus.foodonline.sqlserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJdbctemplateSqlServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJdbctemplateSqlServerApplication.class, args);
+	}
+
+}
